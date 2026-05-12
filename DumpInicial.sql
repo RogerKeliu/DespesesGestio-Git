bc929e5e0f975c3321263ab4ff0291d462c7736d
Keliu <rogermlml18@gmail.com>
2026-05-12 18:30:03 +0200

    fix init db

--- DumpInicial.sql
+++ DumpInicial.sql
@@ -4,49 +4,31 @@
 -- ------------------------------------------------------
 -- Server version	8.0.45
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `categories`
---
 
 DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
+
 CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
+
 
 --
 -- Dumping data for table `categories`
 --
 
 LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+
 INSERT INTO `categories` VALUES (1,'Alimentació'),(2,'Transport'),(3,'Treballadors');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `moviments`
---
 
 DROP TABLE IF EXISTS `moviments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `moviments` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_usuari` int NOT NULL,
@@ -59,26 +41,17 @@ CREATE TABLE `moviments` (
   KEY `fk_moviments_categoria` (`id_categoria`),
   CONSTRAINT `fk_moviments_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categories` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_moviments_usuari` FOREIGN KEY (`id_usuari`) REFERENCES `usuaris` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
---
--- Dumping data for table `moviments`
---
 
 LOCK TABLES `moviments` WRITE;
-/*!40000 ALTER TABLE `moviments` DISABLE KEYS */;
+
 INSERT INTO `moviments` VALUES (1,1,1,'Alimentacio Admin',10.00,'2026-04-11'),(2,2,1,'Alimentacio Roger',9.00,'2026-04-11');
-/*!40000 ALTER TABLE `moviments` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `usuaris`
---
-
 DROP TABLE IF EXISTS `usuaris`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `usuaris` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(255) NOT NULL,
@@ -86,26 +59,15 @@ CREATE TABLE `usuaris` (
   `rol` varchar(255) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`),
   UNIQUE KEY `nom` (`nom`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB;
 
 --
 -- Dumping data for table `usuaris`
 --
 
 LOCK TABLES `usuaris` WRITE;
-/*!40000 ALTER TABLE `usuaris` DISABLE KEYS */;
+
 INSERT INTO `usuaris` VALUES (1,'admin','$2y$10$qDi6zaegCICYldGH7w3dgeb70eQOuwGTeZcgTfosWZqsMNUBkU0n2','admin'),(2,'roger','$2y$10$qDi6zaegCICYldGH7w3dgeb70eQOuwGTeZcgTfosWZqsMNUBkU0n2','user');
-/*!40000 ALTER TABLE `usuaris` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-05-04 23:33:33
